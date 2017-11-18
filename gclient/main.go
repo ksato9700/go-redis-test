c8ec1782b87d9bf49ccaf47371a759b4fabac167
Ken Sato <ksato9700@gmail.com>
2017-11-18 10:10:58 +0900

    initial commit

---
+++ gclient/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+import "github.com/go-redis/redis"
+
+func main() {
+	var err error
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+
+	err = client.Set("key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := client.Get("key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	val2, err := client.Get("key2").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exists")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("key2", val2)
+	}
+	// Output: key value
+	// key2 does not exists
+}
